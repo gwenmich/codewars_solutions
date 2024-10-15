59afcfc93537e063f54b00e53546899e5f5e1f63
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-10-15 20:34:19 +0100

    Updated rock_paper_scissors

--- python_solutions/8_kyu/rock_paper_scissors.py
+++ python_solutions/8_kyu/rock_paper_scissors.py
@@ -2,23 +2,11 @@ def rps(p1, p2):
     if p1 == p2:
         return "Draw!"
 
-    elif p1 == "scissors":
-        if p2 == "paper":
-            return "Player 1 won!"
-        elif p2 == "rock":
-            return "Player 2 won!"
+    elif (p1 == "scissors" and p2 == "paper") or (p1 == "paper" and p2 == "rock") or (p1 == "rock" and p2 == "scissors"):
+        return "Player 1 won!"
 
-    elif p1 == "paper":
-        if p2 == "scissors":
-            return "Player 2 won!"
-        elif p2 == "rock":
-            return "Player 1 won!"
-
-    elif p1 == "rock":
-        if p2 == "scissors":
-            return "Player 1 won!"
-        elif p2 == "paper":
-            return "Player 2 won!"
+    else:
+        return "Player 2 won!"
 
 
 # https://www.codewars.com/kata/5672a98bdbdd995fad00000f/train/python
