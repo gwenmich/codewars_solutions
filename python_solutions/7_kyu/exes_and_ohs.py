c46818b1aac6bd64bb7c92149512f0196c9c92ee
gwenmich <79330147+cgmich@users.noreply.github.com>
2024-10-16 17:49:28 +0100

    7kyu exes_and_ohs solution

---
+++ python_solutions/7_kyu/exes_and_ohs.py
@@ -0,0 +1,8 @@
+def xo(s):
+    if s.lower().count("x") == s.lower().count("o"):
+        return True
+    else:
+        return False
+
+
+# https://www.codewars.com/kata/55908aad6620c066bc00002a/train/python
